e5c9cc0932dac7cb361263758f89e2137e1c5c26
cryptodev35 <cryptodev35@gmail.com>
2015-07-27 16:50:25 +0200

    Update Status.js

--- app/models/Status.js
+++ app/models/Status.js
@@ -2,7 +2,7 @@
 //var imports       = require('soop').imports();
 
 var async     = require('async');
-var bitcore   = require('darkcore');
+var bitcore   = require('bitcore-icash');
 var RpcClient = bitcore.RpcClient;
 var config    = require('../../config/config');
 var rpc       = new RpcClient(config.bitcoind);
